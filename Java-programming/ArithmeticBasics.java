e6b44630f820e6adefeef4cecfb880808b9932b1
EngReteti <amasonlerionka@gmail.com>
2026-01-22 16:28:00 +0300

    Create ArithmeticBasics.java

---
+++ Java-programming/ArithmeticBasics.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Task 1: Hello World and Name
+        System.out.println("Hello World!");
+        System.out.println("My name is Engineer Reteti");
+
+        // Task 2: Simple Arithmetic
+        int a = 15;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("The arithmetic sum is: " + sum);
+    }
+}
